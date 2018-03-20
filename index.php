fdb92d3e09a639a4b8f70bbaae4d9f1a00282d20
iregon <0iregon0@gmail.com>
2018-03-20 12:11:50 +0100

    lettura categorie

--- index.php
+++ index.php
@@ -5,24 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Jokes :: Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/home.css"/>
     <!-- <script src="main.js"></script> -->
 </head>
 <body>
-    <?php
-        include("conn.php");
+    <div class="header"></div>
+    <div class="sidebar">
+        <table class="categoriestable">
+            <tr><th>Categorie</th></tr>
+            <?php
+                include("conn.php");
 
-        $sql = "SELECT j.joketext, j.jokedate , a.name
-                FROM joke j, author a
-                WHERE j.idauthor = a.id";
+                $sql = "SELECT name
+                        FROM category";
 
-        echo "<table>";
-        if($result = $conn->query($sql)) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr><td>".$row['joketext']."</td></tr>";
+                if($result = $conn->query($sql)) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr><td>".$row['name']."</td></tr>";
+                    }
+                }
+            ?>
+        </table>
+    </div>
+    <div class="content">
+        <table class="jokestable">
+        <?php
+            $sql = "SELECT j.joketext, j.jokedate , a.name
+                    FROM joke j, author a
+                    WHERE j.idauthor = a.id";
+
+            if($result = $conn->query($sql)) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr><td>".$row['joketext']."</td></tr>";
+                }
             }
-        }
-        echo "</table>";
-    ?>
+        ?>
+        </table>
+    </div>
+    <div class="footer"></div>
 </body>
 </html>
