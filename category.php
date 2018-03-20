0bc567b0438215b54fa9b909f1aecb87eac61d08
iregon <0iregon0@gmail.com>
2018-03-20 22:26:08 +0100

    aggiunte voci login e registarti al menu

--- category.php
+++ category.php
@@ -19,8 +19,8 @@
             <ul class="navbar">
                 <li><a class="active" href="index.php">Home</a></li>
                 <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
+                <li class="singup"><a href="#about">Registrati</a></li>
+                <li class="login"><a href="#contact">Login</a></li>
             </ul>
         </div>
     </div>
