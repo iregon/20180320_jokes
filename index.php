e5bddf93914d13513b022285bb7b618519b37aaa
iregon <0iregon0@gmail.com>
2018-03-31 16:51:23 +0200

    rimozione funzione aggiornamento autori

--- index.php
+++ index.php
@@ -56,39 +56,6 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script type="text/javascript">
-      function loadAuthors() {
-        var dataList = document.getElementById('json-datalist');
-
-        // Clean datalist options
-        dataList.innerHTML = "";
-        
-        var sugg = document.getElementById("autocomplete").value;
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            var jsonOptions = JSON.parse(this.responseText);
-
-            jsonOptions.forEach(function(item) {
-              // Create a new <option> element.
-              var option = document.createElement('option');
-              // Set the value using the item in the JSON array.
-              option.value = item;
-              // Add the <option> element to the <datalist>.
-              dataList.appendChild(option);
-            });
-            // $( "#autocomplete" ).autocomplete({
-            //   source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
-            // });
-          }
-        };
-
-        // console.log("getAuthors.php?sugg=" + sugg);
-        xhttp.open("GET", "getAuthors.php?sugg=" + sugg, true);
-        xhttp.send();
-      }
-    </script>
 </head>
 <body>
     <div class="header">
